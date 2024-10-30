bcea24f9ff9b16ce673c4cf3215642ca1e9078b0
masakk1 <igotscammedlmfaobutimmasak1@gmail.com>
2024-10-30 20:45:53 +0100

    Update Todos and changed project: Adding more algorithms also added a levenshtein distance+double-metaphone system which ended up being kinda the same

--- TODOs.md
+++ TODOs.md
@@ -1,39 +1,39 @@
 # Goals
 1. [ ] Improving the phonetic search.
-  - Value if it is worth adding a different kind of "type", an "underscore" type. Differentiating them from the "<type>".
-  - Should not take the _ for calculating the metaphonetic code. There will never be a node called "Node_Type" and another "NodeType"
-  - Evaluate wheter to add fuzzy search to the metaphone
-2. [ ] Structure the project
-2. [ ] Check the TODOs for the funnystring-to-json project
-3. [ ] Add command recognition
-  - Bind different aliases. ie: give me a, gimme a, generate a, create node, etc - type, of type, etc.
-  - Must have a "type" identifier
-    - When a type is specified, and the command uses types, return like: ValueAdd<float>
-  - Mush have a "node-creation" command
-  - When sending the commands to resonite, send identifiers instead (ie: new node, generate a, give me a -> NEWNODE)
-5. [ ] Implement a web socked to communicate to Resonite
-  - Check what others have made, perhaps.
-  - Must be able to send the speech in text.
-6. [ ] Create python tests
-  - Record some of the more problematic nodes, and see how goot well they are being handled
-7. [ ] Test under Windows
-8. [ ] Check permormance on different bechmarks as well as in game (VR)
-  - Note: the base model wasn't really that hard to run, but it's good to have numbers
-  - since that may affect some people's decision
-9. [ ] Add a README.md
-10. [ ] Create releases
-  - Try learning github actions
-
-
-# Current project
-1. Improving the phonetic search
-
-## Current tasks
   - [X] Value if it is worth adding a different kind of "type", an "underscore" type. Differentiating them from the "<type>".
   NOTE: no
-
   - [X] Should not take the _ for calculating the metaphonetic code. There will never be a node called "Node_Type" and another "NodeType"
   - [X] Convert numbers to text when calculating metaphone code
   - [ ] Evaluate whether to add fuzzy search to the metaphone
   - [ ] Revise all of the "not detected commands"
   - [ ] fuzzy_search for phonetic_fuzz_search only searches for the nodes, return the whole node
+  - [ ] Make use of metaphone1
+2. [ ] Look into more phonetic algorithms
+3. [ ] Structure the project
+4. [ ] Check the TODOs for the funnystring-to-json project
+5. [ ] Add command recognition
+  - Bind different aliases. ie: give me a, gimme a, generate a, create node, etc - type, of type, etc.
+  - Must have a "type" identifier
+    - When a type is specified, and the command uses types, return like: ValueAdd<float>
+  - Mush have a "node-creation" command
+  - When sending the commands to resonite, send identifiers instead (ie: new node, generate a, give me a -> NEWNODE)
+6. [ ] Implement a web socked to communicate to Resonite
+  - Check what others have made, perhaps.
+  - Must be able to send the speech in text.
+7. [ ] Create python tests
+  - Record some of the more problematic nodes, and see how goot well they are being handled
+8. [ ] Test under Windows
+9. [ ] Check permormance on different bechmarks as well as in game (VR)
+  - Note: the base model wasn't really that hard to run, but it's good to have numbers
+  - since that may affect some people's decision
+10. [ ] Add a README.md
+11. [ ] Create releases
+  - Try learning github actions
+
+
+# Current project
+Looking into more phonetic algorithms
+
+## Tasks
+1. [ ] Create the phonetic algorithms - https://en.wikipedia.org/wiki/Phonetic_algorithm
+2. [ ] Add unit tests
--- speech_to_resonite/src/phonetic_fuzz_search.py
+++ speech_to_resonite/src/phonetic_fuzz_search.py
@@ -1,6 +1,11 @@
 import json
 from rapidfuzz import process
 import metaphone
+import Levenshtein as lev
+
+
+def remove_list_dulicates(ls: list) -> list:
+    return list(set(ls))
 
 
 class PhoneticFuzzSearch:
@@ -24,7 +29,67 @@ class PhoneticFuzzSearch:
     def search_fuzzy(self, query: str, list, limit) -> list:
         return process.extract(query, list, limit=limit)
 
+    def search_node_fuzzy_metaphone(self, query: str, nodes, limit) -> list:
+        """
+        Returns a list of nodes that are the most similar(fuzzy)
+        to the metaphone of the query.
+        """
+        query_metaphone = metaphone.doublemetaphone(query)
+
+        self.debugging_print(f"Query: {query}, metaphone: {query_metaphone}")
+
+        matches_metaphone = self.search_fuzzy(
+            query_metaphone[0], [node["metaphone0"] for node in nodes], limit=limit
+        )
+        matches_metaphone = remove_list_dulicates(matches_metaphone)
+        self.debugging_print("Metaphone matches:", matches_metaphone)
+
+        matching_nodes = []
+        for match in matches_metaphone:
+            code, score, _ = match
+            for node in nodes:
+                if node["metaphone0"] == code:
+                    matching_nodes.append(node)
+
+        return matching_nodes
+
+    def match_lev_double_metaphone(
+        self, similarity_theshold, query1, query2, target1, target2
+    ):
+        dist11 = lev.distance(query1, target1)
+        dist12 = lev.distance(query1, target2)
+        dist21 = lev.distance(query2, target1)
+        dist22 = lev.distance(query2, target2)
+
+        weighted_distance = (dist11 + dist12 + dist21 + dist22) / 4
+
+        return weighted_distance <= similarity_theshold, weighted_distance
+
+    def search_node_lev_double_metaphone(self, query: str, nodes, limit) -> list:
+        """
+        Returns a list fo nodes that are most similar(levenshtein distance)
+        to the metaphone(s) of the query.
+        """
+        query1, query2 = metaphone.doublemetaphone(query)
+
+        self.debugging_print(f"Query: {query}, metaphone: {query1}, {query2}")
+
+        matches = []
+        for node in nodes:
+            node1, node2 = node["metaphone0"], node["metaphone1"]
+            is_match, distance = self.match_lev_double_metaphone(
+                3, query1, query2, node1, node2
+            )
+            if is_match:
+                matches.append(node)
+
+        return matches
+
     def search_node_metaphone(self, query: str, list, limit=5) -> list:
+        """
+        Returns a list of nodes that exactly match
+        the metaphone of the query.
+        """
         query_metaphone = metaphone.doublemetaphone(query)
 
         self.debugging_print(f"Query: {query}, metaphone: {query_metaphone}")
@@ -40,6 +105,12 @@ class PhoneticFuzzSearch:
                 )
                 matches.append(item)
                 index += 1
+            if item["metaphone1"] != "" and item["metaphone1"] == query_metaphone[1]:
+                self.debugging_print(
+                    f"looking at {item['name']} - metaphone: {item['metaphone1']}"
+                )
+                matches.append(item)
+                index += 1
 
         return matches
 
@@ -47,16 +118,16 @@ class PhoneticFuzzSearch:
         query = query.lower()
         found_node = None
 
-        metaphone_matches = self.search_node_metaphone(query, self.nodes, 5)
-        self.debugging_print("Phonetic matches:", metaphone_matches)
-        if metaphone_matches == []:
+        node_matches = self.search_node_lev_double_metaphone(query, self.nodes, 5)
+        self.debugging_print("Node matches:", node_matches)
+        if node_matches == []:
             self.debugging_print(
                 f"No metaphone matches found for {query} ({metaphone.dm(query)})"
             )
             return None
 
         fuzzy_matches = self.search_fuzzy(
-            query, [node["name"].lower() for node in metaphone_matches], limit=20
+            query, [node["name"].lower() for node in node_matches], limit=20
         )
         self.debugging_print("Fuzzy matches:", fuzzy_matches)
         found_node = fuzzy_matches[0]
